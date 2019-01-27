ecbc10a9dab1496da5e7cb591e938493c19443b6
Matt Portwood <portwood.matt@gmail.com>
2019-01-26 22:07:10 -0800

    created a function to call from Jupyter

--- test_script.py
+++ test_script.py
@@ -1,6 +1,6 @@
 # in test_script.py
-def hello_script(name): 
+def hello_script(): 
     """This function takes no inputs and prints 2 lines"""
-    print("Hello", name, ", from the command line!")
+    print("Hello from the command line!")
     print("It is a good day to script.")
-print(hello_script("Matt"))
+print(hello_script())
