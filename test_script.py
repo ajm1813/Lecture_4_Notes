85724d82fc6bfd9e74a744ac32bb01af4e03b880
Matt Portwood <portwood.matt@gmail.com>
2019-01-26 22:25:55 -0800

    set up script with main program syntax

--- test_script.py
+++ test_script.py
@@ -1,6 +1,10 @@
 # in test_script.py
-def hello_script(): 
+def hello_script(name): 
     """This function takes no inputs and prints 2 lines"""
-    print("Hello from the command line!")
+    print("Hello from the command line " + name + "!")
     print("It is a good day to script.")
-print(hello_script())
+
+if __name__ == "__main__":
+    print("Executing as main program")
+    print(hello_script("class"))
+    print("Done Executing Script. Have a Nice Day")
