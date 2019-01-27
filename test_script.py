6bf59c0b905588ec4d37ae3072c8a272e210009d
Matt Portwood <portwood.matt@gmail.com>
2019-01-26 21:59:16 -0800

    add test for python/jupyter module

--- test_script.py
+++ test_script.py
@@ -1,7 +1,6 @@
-# in script.py
-def hello_script(): 
+# in test_script.py
+def hello_script(name): 
     """This function takes no inputs and prints 2 lines"""
-    print("Hello from the command line!")
+    print("Hello", name, ", from the command line!")
     print("It is a good day to script.")
-
-print(hello_script())
+print(hello_script("Matt"))
