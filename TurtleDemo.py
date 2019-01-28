7c32b366e59bb4a0cef03a8aef848264f5d73019
Matt Portwood <portwood.matt@gmail.com>
2019-01-27 16:05:46 -0800

    add loop to demo turtle

--- TurtleDemo.py
+++ TurtleDemo.py
@@ -25,16 +25,16 @@ my_turtle.speed("slow")  # how fast the turtle should move
 # my_turtle.pendown()
 # my_turtle.forward(WINDOW_WIDTH - 50)
 
-##Move the turtle to draw
-my_turtle.home()
-my_turtle.penup()       # do not draw while moving
-my_turtle.goto(30, 60)  # walk to coordinates
-my_turtle.pendown()     # start drawing again
-my_turtle.forward(80)   # move forward
-my_turtle.left(60)      # turn left
-my_turtle.forward(120)  # move forward
-my_turtle.right(120)    # turn left
-my_turtle.forward(120)  # move forward
+# ##Move the turtle to draw
+# my_turtle.home()
+# my_turtle.penup()       # do not draw while moving
+# my_turtle.goto(30, 60)  # walk to coordinates
+# my_turtle.pendown()     # start drawing again
+# my_turtle.forward(80)   # move forward
+# my_turtle.left(60)      # turn left
+# my_turtle.forward(120)  # move forward
+# my_turtle.right(120)    # turn left
+# my_turtle.forward(120)  # move forward
 
 # my_turtle.home()
 # my_turtle.pendown()       # do not draw while moving
@@ -47,7 +47,25 @@ my_turtle.forward(120)  # move forward
 # my_turtle.right(90)    # turn left
 # my_turtle.forward(20)
 
-
+filename = 'data/sample_data.txt'
+with open(filename) as file: 
+    count = 0
+    for line in file: 
+        my_turtle.color("purple")
+        my_turtle.pendown()
+        my_turtle.begin_fill()
+        my_turtle.left(90)
+        my_turtle.forward(100)
+        my_turtle.right(90)
+        my_turtle.forward(20)
+        my_turtle.right(90)
+        my_turtle.forward(100)
+        my_turtle.right(90)
+        my_turtle.forward(20)
+        my_turtle.end_fill()
+        my_turtle.penup()
+        my_turtle.right(180)
+        my_turtle.forward(20 + 10)
 
 # Make turtle graphics appear and run; wait for the user to close the screen
 # This MUST be the last statement executed in the script
